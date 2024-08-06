eabce04b1ed5875524e6b17c145eeecd176c9000
FarmerJohn-Git <150650723+FarmerJohn-Git@users.noreply.github.com>
2024-08-06 19:24:33 +0800

    Update and rename Langsy-1 to Temp.php
    表单处理

---
+++ Temp.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset = "utf-8">
+  <title>测试</title>
+</head>
+
+<?php
+  function turn(){
+    $url = "http://localhost/test.html"; 
+    echo "<meta http-equiv='refresh' content ='2;url=$url'>";
+  }
+
+  if(isset($_GET["NUM1"]) and isset($_GET["NUM2"])){
+    echo "<h3><font color = green><strong>测试成功</strong></font></h3>";
+    $a = $_GET["NUM1"];
+    $b = $_GET["NUM2"];
+    if(!is_numeric($a) or !is_numeric($b)){
+      turn();
+      die("<br><font color = \"#FF0000\">Error: 输入错误</font>");
+    }else{
+      $a = intval($a);
+      $b = intval($b);
+      echo "<br>MIN:". ($a < $b ? $a : $b);
+    }  
+  }else echo "<strong>测试失败</strong>";
+
+  echo "<br><a href = \"http://localhost/test.html\">返回上一页</a>"
+?>
+
+</html>
